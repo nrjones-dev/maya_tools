25341182e521f3baeadd477153ae770aeac6f9a9
Nathan Jones <115369332+nrjones-dev@users.noreply.github.com>
2025-12-01 15:32:56 +0000

    updates init

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from modules.tools.tools import (
+from .modules.tools.tools import (
     clean_combine,
     clean_detach,
     select_every_other_face,
@@ -6,7 +6,7 @@ from modules.tools.tools import (
     set_obj_world_space,
     set_pivot_world_space,
 )
-from modules.utils.renderutils import (
+from .modules.utils.renderutils import (
     append_diffuse_to_render_layers,
     count_render_layers,
 )
